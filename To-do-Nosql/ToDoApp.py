1fb4389eb295824dec8bd93663e48682391647c5
aquibali01 <aquibali.92@gmail.com>
2019-12-11 00:45:12 +0500

    Basic app created and a task is added in database

---
+++ To-do-Nosql/ToDoApp.py
@@ -0,0 +1,27 @@
+# importing all the libraries
+
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo 
+
+app = Flask(__name__)
+
+#connecting to the mongodb atlas
+app.config["MONGO_URI"] = "mongodb+srv://m220student:m220password@flask-wrhs8.mongodb.net/test?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
+# Adding the Task to the database
+
+@app.route("/addTasks")
+def addTasks():
+    task = mongo.db.toDoApp
+    task.insert(
+        {"id" : 0,
+        "title" : "Meeting with client",
+        "description" : "The Meeting is with Mr. Ali @ 11 A.M",
+        "done": False}
+        )
+    return "Task(s) Added"
+
+if __name__ == "__main__":
+    app.run(debug=True)
